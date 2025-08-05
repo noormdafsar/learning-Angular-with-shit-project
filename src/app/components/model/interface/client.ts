b4dd84dfda74bf4720e6d7459c234f0cadf443d3
Nooruddin Md Afsar <mdafsar786noor@gmail.com>
2025-08-05 18:59:22 +0530

    employee and client interface created

---
+++ src/app/components/model/interface/client.ts
@@ -0,0 +1,15 @@
+export interface IClient {
+    clientId: number,
+    clientName: string,
+    companyName: string,
+    address: string,
+    contactNo: number,
+}
+
+export interface IEmpoyee {
+    employeeId: number,
+    employeeName: string,
+    companyName: string,
+    address: string,
+    contactNo: number,
+}
