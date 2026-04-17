aac0d9e4563eefa1b9ece487efc8253f263acf43
Nooruddin Md Afsar <mdafsar786noor@gmail.com>
2026-04-17 19:18:39 +0530

    fix angular file replacement during build

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -5,13 +5,17 @@ set -e
 ENV_FILE_DIR="src/environments"
 [ -d "$ENV_FILE_DIR" ] || mkdir -p "$ENV_FILE_DIR"
 
-FILE_PATH="src/environments/environment.prod.ts"
+PROD_ENV_FILE_PATH="src/environments/environment.prod.ts"
+ENV_FILE_PATH="src/environments/environment.ts"
 
 echo "Preparing environment file..."
 
-[ -f "$FILE_PATH" ] && rm "$FILE_PATH"
+[ -f "$PROD_ENV_FILE_PATH" ] && rm "$PROD_ENV_FILE_PATH"
 
-cat <<EOF > $FILE_PATH
+# Create an dummy environment file if it doesn't exist, otherwise angular file replacement will fail
+[ -f "$ENV_FILE_PATH" ] && cat "$ENV_FILE_PATH"
+
+cat <<EOF > $PROD_ENV_FILE_PATH
 export const environment = {
   production: true,
   apiUrl: '${API_URL}',
