c42b510b052049ceb6e5e2677809882bbe45f88e
Nooruddin Md Afsar <mdafsar786noor@gmail.com>
2026-04-17 19:23:05 +0530

    fix env file creation

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -5,17 +5,11 @@ set -e
 ENV_FILE_DIR="src/environments"
 [ -d "$ENV_FILE_DIR" ] || mkdir -p "$ENV_FILE_DIR"
 
-PROD_ENV_FILE_PATH="src/environments/environment.prod.ts"
 ENV_FILE_PATH="src/environments/environment.ts"
 
 echo "Preparing environment file..."
 
-[ -f "$PROD_ENV_FILE_PATH" ] && rm "$PROD_ENV_FILE_PATH"
-
-# Create an dummy environment file if it doesn't exist, otherwise angular file replacement will fail
-[ -f "$ENV_FILE_PATH" ] && cat "$ENV_FILE_PATH"
-
-cat <<EOF > $PROD_ENV_FILE_PATH
+cat <<EOF > $ENV_FILE_PATH
 export const environment = {
   production: true,
   apiUrl: '${API_URL}',
