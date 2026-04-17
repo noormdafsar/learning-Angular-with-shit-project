506f10c076a4d93c8a881d40a270f7b6b0b0582e
Nooruddin Md Afsar <mdafsar786noor@gmail.com>
2026-04-17 16:35:56 +0530

    environment example file uploaded

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: '',
+  apiUrl: '',
+  appType: '',
+  appVersion: '',
+  enableDebugMode: '',
+  enableConsoleLogs: '',
+  encryptionEnabled: '',
+  tokenRefreshThreshold: '', // minutes before expiry to refresh token
+};
